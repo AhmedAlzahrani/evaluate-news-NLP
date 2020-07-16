858d106f947b110b0e7980ca60f1cb03700fbf1d
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-07-17 01:29:21 +0300

    webpack: Ex2

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,11 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+
+const PORT = 1200;
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
 })
 
 app.get('/test', function (req, res) {
