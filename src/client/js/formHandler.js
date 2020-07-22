24a058e57565aea30dcb5950b96e63a928fe4ff7
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-07-22 04:55:52 +0300

    OpenWeatherMap API Call has been added and worksgit add .

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,8 @@
+//Global variables
+const API_KEY = `&appid=9a134735d7359ef641e698a70b098619`;
+const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=`;
+
+
 export function handleSubmit(event) {
     event.preventDefault()
 
@@ -6,10 +11,17 @@ export function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(`${API_URL}${formText}${API_KEY}`)
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then(res => {
+        console.log(res);
+        console.log(res.main.temp);
+        document.getElementById('results').textContent = `Temp: ${res.main.temp}`;
     })
+    //http://localhost:8081/api.openweathermap.org/data/2.5/weather?q=London&appid=9a134735d7359ef641e698a70b098619
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
 }
-// export { handleSubmit , foo }
