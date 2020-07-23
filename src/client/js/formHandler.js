1a7d0c8f6c8cf0021b7c970cb1b36ec46f346b08
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-07-23 11:17:42 +0300

    new repo intialization ann initial commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -18,10 +18,4 @@ export function handleSubmit(event) {
         console.log(res.main.temp);
         document.getElementById('results').textContent = `Temp: ${res.main.temp}`;
     })
-    //http://localhost:8081/api.openweathermap.org/data/2.5/weather?q=London&appid=9a134735d7359ef641e698a70b098619
-    // fetch('http://localhost:8081/test')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.message
-    // })
 }
